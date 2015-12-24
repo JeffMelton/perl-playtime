6bfbdeb2359824cea6d809dd5e0df6e28d120b53
Jeff Melton <jeff@themeltonplantation.com>
2015-12-23 21:45:08 -0600

    Script now splits input file into 2 CSVs

--- csv-convert.pm
+++ csv-convert.pm
@@ -8,19 +8,43 @@ use Excel::Writer::XLSX;
 
 my $file = $ARGV[0] or die "Please supply a CSV file as a script argument.\n";
 my ( $out1, $out2, $out3 );
-my $match1 = "Patch Installation Status";
-my $match2 = "Number of Modified Patches by Classification";
-my $csv1   = $match1 . ".csv";
-my $csv2   = $match2 . ".csv";
+my ( @csv0, @csv1, @csv2 );
+my $worksheet1 = "Patch Installation Status";
+my $worksheet2 = "Number of Modified Patches by Classification";
+my $csv1   = $worksheet1 . ".csv";
+my $csv2   = $worksheet2 . ".csv";
 my $date   = strftime '%Y-%m-%d', localtime;
 my $xlsx   = "patch_status_" . $date . ".xlsx";
 
+# DO NOT uncomment this until ready to roll!
+# system("sed -i.bak -e '1,22d' patch_status.csv");
+
 # open input file,reading backwards
-#tie *BW, 'File::ReadBackwards', $file or die "Could not open '$file' $!\n";
+tie *BW, 'File::ReadBackwards', $file or die "Could not open '$file' $!\n";
 
-#my $bw = File::ReadBackwards->new($file) or die "Could not open '$file' $!\n";
-
-# open two output files
-#open $out1, '>', "$csv1" or die "Could not open '$csv1' $!\n";
-#open $out2, '>', "$csv2" or die "Could not open '$csv2' $!\n";
+# open output files
+open $out1, '>', "$csv1" or die "Could not open '$csv1' $!\n";
+open $out2, '>', "$csv2" or die "Could not open '$csv2' $!\n";
 #open $out3, '>', "$xlsx" or die "Could not open '$xlsx' $!\n";
+
+while (my $line = <BW>) {
+	unless ($line =~ /^$/) {
+		chomp($line);
+		push (@csv0, $line);
+	}
+}
+
+my $csv0 = join("\n", @csv0);
+my @blocks = split(/$worksheet1/, $csv0);
+@csv1 = split("\n", $blocks[0]);
+@csv2 = split("\n", $blocks[1]);
+@csv1 = reverse(@csv1);
+@csv2 = reverse(@csv2);
+foreach (@csv1) {
+	print $out1 "$_\n";
+}
+foreach (@csv2) {
+	unless ($_ =~ /$worksheet2/) {
+		print $out2 "$_\n";
+	}
+}

8b5b5b86355c2fb29d53dd3eb90c485962bd96b1
Jeff Melton <jeff@themeltonplantation.com>
2016-09-20 12:21:05 -0500

    Update to texapp forecast extension
    -	new API endpoint

--- texapp-forecast.pm
+++ texapp-forecast.pm
@@ -9,7 +9,7 @@ $handle = sub {
     my $api_key = "YOUR API KEY HERE";
     my $data =
       &grabjson(
-        "https://api.forecast.io/forecast/${api_key}/${lat},${long},${time}");
+        "https://api.darksky.net/forecast/${api_key}/${lat},${long},${time}");
     my $current_conditions = $data->{'currently'}->{'summary'};
     my $current_temp       = $data->{'currently'}->{'temperature'};
     &std(
